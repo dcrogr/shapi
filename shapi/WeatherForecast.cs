76ff31f329ca75cf52b9598f121e3b084b94cd71
dcrogr <dockerogreniyorum@gmail.com>
2022-01-31 10:23:28 +0300

    s

--- shapi/WeatherForecast.cs
+++ shapi/WeatherForecast.cs
@@ -12,7 +12,7 @@ namespace shapi
 
         public string Summary { get; set; }
         public string IsAlive { get; set; } = "Çok Şükür!!";
-        public string NAme { get; set; } = "Hkn";
+        public string NAme { get; set; } = "Hkns";
 
 
 

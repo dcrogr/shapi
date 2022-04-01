fdf9f57387060429fd4e3c4d8180b8a2c737fa6d
dcrogr <dockerogreniyorum@gmail.com>
2022-04-01 17:22:10 +0300

    Update WeatherForecastController.cs

--- shapi/Controllers/WeatherForecastController.cs
+++ shapi/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace shapi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "test", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
